5e3aa86b77d3fbf68204637ee9fedb422a27868e
Gore <ayush.gore@wolterskluwer.com>
2023-09-01 10:33:38 +0530

    fixed the bug

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-
-import { useState } from 'react';
-import './App.css';
-import {File} from './Component/File'
-import {Folder} from './Component/Folder'
-import { FileExp } from './Component/FileExp';
+import { useState } from "react";
+import "./App.css";
+import { FileExp } from "./Component/FileExp";
 
 function App() {
-  const[inputName,setInputName]=useState("");
-  const [store,setStore]=useState([{
-    id:0,
-    name:"Root",
-    type:"folder",
-    children:[],
-    deleted:false
-  }
-])
-const [render, setRender] = useState(false);
-  console.log("hii")
+  const [inputName, setInputName] = useState("");
+  const [store, setStore] = useState([
+    {
+      id: 0,
+      name: "Root",
+      type: "folder",
+      children: [],
+      deleted: false,
+    },
+  ]);
+  const [render, setRender] = useState(false);
+  console.log("hii");
   return (
     <div className="App">
-      <div className='header' style={{marginInline:"20px"}}>
-        <h1 className='heading'>React File Explorer</h1>
-        <p style={{display:"inline-block",marginRight:"10px"}}>Enter File/Folder Name:</p>
-        <input type='text' placeholder='Enter File/Folder Name' value={inputName} onChange={(e)=>{setInputName(e.target.value)}}/>
+      <div className="header" style={{ marginInline: "20px" }}>
+        <h1 className="heading">React File Explorer</h1>
+        <p style={{ display: "inline-block", marginRight: "10px" }}>
+          Enter File/Folder Name:
+        </p>
+        <input
+          type="text"
+          placeholder="Enter File/Folder Name"
+          value={inputName}
+          onChange={(e) => {
+            setInputName(e.target.value);
+          }}
+        />
         <hr></hr>
       </div>
-      <div className='body'>
-        <FileExp store={store} inputName={inputName} setInputName={setInputName} /> 
+      <div className="body">
+        <FileExp
+          store={store}
+          inputName={inputName}
+          setInputName={setInputName}
+        />
       </div>
     </div>
   );
--- src/Component/File.js
+++ src/Component/File.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
 
-export const File=(props)=>{
+export const File = (props) => {
+  const deleteNode = (store, id) => {
+    store.map((el) => {
+      if (el.id === id) {
+        el.deleted = true;
+        props.setInputName("");
+        console.log("deleted.");
+      } else if (el.children != null) {
+        deleteNode(el.children, id);
+      }
+    });
+  };
   return (
     <div>
-        <label style={{margin:"3px"}}>📝{props.inputName}</label>
-        <button style={{margin:"3px",borderRadius:"20px"}} onClick={()=> deleteNode(props.store,props.id)}>🗑</button>
+      <label style={{ margin: "3px" }}>📝{props.inputName}</label>
+      <button
+        style={{ margin: "3px", borderRadius: "20px" }}
+        onClick={() => deleteNode(props.store, props.id)}
+      >
+        🗑
+      </button>
     </div>
-  )
-}
+  );
+};
